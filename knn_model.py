f24e9f5ba3452635faf39017dee3bbc5579942ff
Jefferson Lin <jeff3rson110@gmail.com>
2025-11-21 15:02:06 -0500

    .

--- knn_model.py
+++ knn_model.py
@@ -1,6 +1,11 @@
 # training file for the knn model
 
 from data.data_reader import load_all_data, load_dataset
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def run_knn():
     data = load_all_data()
